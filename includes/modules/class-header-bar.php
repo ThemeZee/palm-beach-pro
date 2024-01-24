d7f6f06bc000aac227d74fc43c59ac4c98239553
Thomas Weichselbaumer <t.weichselbaumer@inpsyde.com>
2024-01-24 14:57:09 +0100

    added X to social icons menu

--- includes/modules/class-header-bar.php
+++ includes/modules/class-header-bar.php
@@ -167,7 +167,7 @@ class Palm_Beach_Pro_Header_Bar {
 
 		// Create SVG markup.
 		$svg  = '<svg class="icon icon-' . esc_attr( $icon ) . '" aria-hidden="true" role="img">';
-		$svg .= ' <use xlink:href="' . PALM_BEACH_PRO_PLUGIN_URL . 'assets/icons/social-icons.svg?ver=20221122#icon-' . esc_html( $icon ) . '"></use> ';
+		$svg .= ' <use xlink:href="' . PALM_BEACH_PRO_PLUGIN_URL . 'assets/icons/social-icons.svg?ver=20240124#icon-' . esc_html( $icon ) . '"></use> ';
 		$svg .= '</svg>';
 
 		return $svg;
@@ -231,6 +231,7 @@ class Palm_Beach_Pro_Header_Bar {
 			'wa.me'           => 'whatsapp',
 			'wordpress.org'   => 'wordpress',
 			'wordpress.com'   => 'wordpress',
+			'x.com'           => 'x-twitter',
 			'xing.com'        => 'xing',
 			'yelp.com'        => 'yelp',
 			'youtube.com'     => 'youtube',
